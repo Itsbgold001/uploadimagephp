f0cfe7739521ec190f47499adfe924e7989b21c1
facunq <facunquintana@gmail.com>
2018-01-09 18:03:12 -0300

    working

--- upload.php
+++ upload.php
@@ -1,99 +1,136 @@
 <?php 
  
-//importing dbconfig file 
-require_once 'dbconfig.php';
+ //importing dbDetails file 
+ require_once 'dbconfig.php';
  
-//this is our upload folder 
-$upload_folder = 'img/';        
+ if($_SERVER['REQUEST_METHOD']=='POST'){
  
-if($_SERVER['REQUEST_METHOD']=='POST'){
+ //checking the required parameters from the request 
+ if(isset($_FILES['image']['name'])){
  
-//checking the required parameters from the request 
-if(isset($_FILES['image']['name'])){
-  
-    //getting file info from the request 
-    $fileinfo = pathinfo($_FILES['image']['name']);
  
-    //getting the file extension 
-    $extension = $fileinfo['extension'];
+ //getting file info from the request 
+ $fileinfo = pathinfo($_FILES['image']['name']);
+ 
+ //getting the file extension 
+ $extension = $fileinfo['extension'];
+ 
 
-    if (($extension != "jpg") && ($extension != "jpeg") && ($extension != "png") && ($extension != "gif"))
- 	    {
-            echo 'Unknown image format';
- 		    $errors=1;
-        }
-
-    //jpg-jpeg     
-    if($extension=="jpg" || $extension=="jpeg" )
-        {
-            $uploadedfile = $_FILES['image']['tmp_name'];
-            $src = imagecreatefromjpeg($uploadedfile);
-            list($width,$height)=getimagesize($uploadedfile);
-            
-            //set the width for adjustment
-            $newwidth=350;
-            $newheight=($height/$width)*$newwidth;
-            $tmp=imagecreatetruecolor($newwidth,$newheight);
-                
-            imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height);
-        
-            //random filename
-            $temp = explode(".", $_FILES["image"]["name"]);
-            $newfilename = round(microtime(true)) . '.' . end($temp);
-                
-            $filename = $upload_folder. $newfilename;
-                    
-            imagejpeg($tmp,$filename,100);
-        
-            imagedestroy($src);
-            imagedestroy($tmp);
-
-            //insert db
-            $insert=mysqli_query($con, "INSERT INTO images (img) VALUES ($filename);");
+ if (($extension != "jpg") && ($extension != "jpeg") && ($extension != "png") && ($extension != "gif"))
+ 	{
+        echo 'Unknown image format.';
     }
 
-    //png
-    else if($extension=="png")
-        {
-            $uploadedfile = $_FILES['image']['tmp_name'];
-            $src = imagecreatefrompng($uploadedfile);
-            list($width,$height)=getimagesize($uploadedfile);
-
-            //set the width for adjustment            
-            $newwidth=350;
-            $newheight=($height/$width)*$newwidth;
-            $tmp=imagecreatetruecolor($newwidth,$newheight);
-                
-            imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height);
-           
-            //random filename        
-            $temp = explode(".", $_FILES["image"]["name"]);
-            $newfilename = round(microtime(true)) . '.' . end($temp);
-                
-            $filename = $upload_folder. $newfilename;
-                    
-            imagejpeg($tmp,$filename,100);
+//jpg-jpeg     
+if($extension=="jpg" || $extension=="jpeg" )
+    {
+        $uploadedfile = $_FILES['image']['tmp_name'];
+        $src = imagecreatefromjpeg($uploadedfile);
+        list($width,$height)=getimagesize($uploadedfile);
         
-            imagedestroy($src);
-            imagedestroy($tmp);
-            
-            //insert db
-            $insert=mysqli_query($con, "INSERT INTO images (img) VALUES ($filename);");
-    }    
-    //gif
-    else
-        {
-            $uploadedfile=$_FILES['image']['tmp_name'];
-            //random filename
-            $temp = explode(".", $_FILES["image"]["name"]);
-            $newfilename = round(microtime(true)) . '.' . end($temp);
+        //set new width
+        $newwidth1=350;
+        $newheight1=($height/$width)*$newwidth1;
+        $tmp1=imagecreatetruecolor($newwidth1,$newheight1);
                 
-            $filename = $upload_folder. $newfilename;
+        imagecopyresampled($tmp1,$src,0,0,0,0,$newwidth1,$newheight1,$width,$height);
 
-            move_uploaded_file($uploadedfile,$upload_folder."/".$newfilename);
+        //new random name        
+        $temp = explode(".", $_FILES["image"]["name"]);
+        $newfilename = round(microtime(true)) . '.' . end($temp);
+                
+        $filename1 = "img/". $newfilename;
+                    
+        imagejpeg($tmp1,$filename1,100);
+        
+        imagedestroy($src);
+        imagedestroy($tmp1);
+        //insert in database
+        $insert=mysqli_query($con, "INSERT INTO images (img) VALUES ('$filename1');");
 
-            //insert db
-            $insert=mysqli_query($con, "INSERT INTO images (img) VALUES ($filename);");
-        }
+        echo "<html>
+		<head>
+		</head>
+		<body>
+			<meta http-equiv='REFRESH' content='0 ; url=index.php'>
+			<script>
+				alert('The image has been uploaded .');
+			</script>
+		</body>
+        </html>";
+    }
+
+//png
+    else if($extension=="png")
+    {
+        $uploadedfile = $_FILES['image']['tmp_name'];
+        $src = imagecreatefrompng($uploadedfile);
+        list($width,$height)=getimagesize($uploadedfile);
+
+        //set new width            
+        $newwidth1=350;
+        $newheight1=($height/$width)*$newwidth1;
+        $tmp1=imagecreatetruecolor($newwidth1,$newheight1);
+                
+        imagecopyresampled($tmp1,$src,0,0,0,0,$newwidth1,$newheight1,$width,$height);
+        
+        //new random name
+        $temp = explode(".", $_FILES["image"]["name"]);
+        $newfilename = round(microtime(true)) . '.' . end($temp);
+                
+        $filename1 = "img/". $newfilename;
+                    
+        imagejpeg($tmp1,$filename1,100);
+        
+        imagedestroy($src);
+        imagedestroy($tmp1);
+
+        //insert in database
+        $insert=mysqli_query($con, "INSERT INTO images (img) VALUES ('$filename1');");
+
+        echo "<html>
+		<head>
+		</head>
+		<body>
+			<meta http-equiv='REFRESH' content='0 ; url=index.php'>
+			<script>
+				alert('The image has been uploaded .');
+			</script>
+		</body>
+        </html>";    
+    }    
+    else if($extension=="gif") {
+    $uploadedfile = $_FILES['image']['tmp_name'];
+    
+    //new random name
+
+    $temp = explode(".", $_FILES["image"]["name"]);
+    $newfilename = round(microtime(true)) . '.' . end($temp);
+                
+    $filename1 = "img/". $newfilename;
+
+	move_uploaded_file($uploadedfile,$filename1);
+
+    //insert in database
+    $insert=mysqli_query($con, "INSERT INTO images (img) VALUES ('$filename1');");
+    
+
+    echo "<html>
+    <head>
+    </head>
+    <body>
+        <meta http-equiv='REFRESH' content='0 ; url=index.php'>
+        <script>
+            alert('The image has been uploaded .');
+        </script>
+    </body>
+    </html>";
+    }
+    else    
+    {
+        echo 'error';
+    }
+
 }
 }
+
